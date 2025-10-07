82a1076e5a5c196eb78d342a001d2b11d1eb3fb1
Durgam Prakash <durgamprakash@Durgams-Air.domain.name>
2025-10-07 15:51:27 +0530

    password encryption added

--- src/main/java/com/software/dpweb/service/AuthService.java
+++ src/main/java/com/software/dpweb/service/AuthService.java
@@ -3,6 +3,8 @@ package com.software.dpweb.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.software.dpweb.entity.User;
@@ -15,6 +17,7 @@ public class AuthService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	public Object createAccount(SignupAPIData signupAPIData) throws Exception {
 		
@@ -24,7 +27,7 @@ public class AuthService {
 			User user = new User();
 			user.setName(signupAPIData.getName());
 			user.setEmail(signupAPIData.getEmail());
-			user.setPassword(signupAPIData.getPassword());
+			user.setPassword(passwordEncoder.encode(signupAPIData.getPassword()));
 			user.setMobile(signupAPIData.getMobile());
 			User dbUserData = userRepository.save(user);
 			return dbUserData;
