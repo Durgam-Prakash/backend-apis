eb41a62fb668127ad3d7edf06d1a33d6baaf2543
Durgam Prakash <durgamprakash@Durgams-Air.domain.name>
2025-10-08 14:21:08 +0530

    file upload api added

---
+++ src/main/java/com/software/dpweb/controller/FileUploadController.java
@@ -0,0 +1,59 @@
+package com.software.dpweb.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.software.dpweb.service.FileUploadService;
+
+@RestController
+@RequestMapping("/api/upload")
+public class FileUploadController {
+	
+	@Autowired
+	private FileUploadService fileUploadService;
+
+	@PostMapping("/images")
+	public ResponseEntity<?> uploadImages(@RequestParam("input_file") MultipartFile inputFile) throws Exception{
+		
+		
+		fileUploadService.imageUpload(inputFile);
+		
+		Map<String,Object> responseMap = new HashMap<>();
+		responseMap.put("Result", "Success");
+		responseMap.put("Message", "image uploaded Successfully....!");
+		//responseMap.put("Data", inputFile);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+
+		
+		
+	}
+	
+	
+	
+	
+	@PostMapping("/pdfs")
+	public ResponseEntity<?> uploadpdfs(@RequestParam("input_file") MultipartFile file) throws Exception{
+		
+		fileUploadService.pdfUploads(file);
+		Map<String,Object> responseMap = new HashMap<>();
+		responseMap.put("Result", "Success");
+		responseMap.put("Message", "pdf file uploaded Successfully....!");
+		responseMap.put("Data", file);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+
+		
+		
+	}
+	
+}
---
+++ src/main/java/com/software/dpweb/service/FileUploadService.java
@@ -0,0 +1,76 @@
+package com.software.dpweb.service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService {
+	
+	@Value("${file.upload.images.path}")
+	String IMAGE_UPLOAD_PATH;
+	
+	
+	@Value("${file.upload.pdfs.path}")
+	String PDF_UPLOAD_PATH;
+	
+	private int MAX_ALLOWED_IMAGE_SIZE = 5 * 1024 * 1024;
+	private int MAX_ALLOWED_PDF_SIZE = 10 * 1024 * 1024;
+	
+	
+	
+	public void imageUpload(MultipartFile inputFile) throws Exception {
+		
+		String fileName = StringUtils.cleanPath(inputFile.getOriginalFilename());
+		String fileType = StringUtils.getFilenameExtension(fileName);
+		
+		
+		String[] allowedFileTypes= {"png","jpeg","jpg","gif"};
+		Boolean isFileTypeAllowed = Arrays.stream(allowedFileTypes).anyMatch(fileType::equals);
+		
+		if(isFileTypeAllowed==false) {
+			throw new Exception(fileType + " file is not allowed");
+		}
+		
+		if(inputFile.getSize()> MAX_ALLOWED_IMAGE_SIZE) {
+			throw new Exception("Max 5MB Allowed");
+		}
+		
+		
+		String uploadImageName = UUID.randomUUID().toString() + " ." + fileType;
+		
+		Path uploadPath = Paths.get(IMAGE_UPLOAD_PATH +uploadImageName);
+		Files.copy(inputFile.getInputStream(), uploadPath);
+		
+		
+	}
+	
+	
+	public void pdfUploads(MultipartFile inputFile) throws Exception {
+		
+		String fileName = StringUtils.cleanPath(inputFile.getOriginalFilename());
+		String fileType = StringUtils.getFilenameExtension(fileName);
+		
+		if(fileType.equals("pdf")==false) {
+			throw new Exception(fileType +" file is not allowed");
+		}
+		
+		if(inputFile.getSize()> MAX_ALLOWED_PDF_SIZE) {
+			throw new Exception("Max 10MB Allowed");
+		}
+		
+		String uploadPdfFileName = UUID.randomUUID().toString() + " .pdf"  ;
+		
+		Path uploadPath = Paths.get(PDF_UPLOAD_PATH +uploadPdfFileName);
+		Files.copy(inputFile.getInputStream(), uploadPath);
+		
+	}
+
+}
